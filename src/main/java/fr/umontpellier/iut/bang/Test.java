054625cc5907d0f53adbd32c7a7c819ac183dfd3
leina.sadi@etu.umontpellier.fr <78019069+leina-sadi@users.noreply.github.com>
2021-05-27 16:54:56 +0200

    titre+logo pour la page ACCUEIL dans Test et des sources

--- src/main/java/fr/umontpellier/iut/bang/Test.java
+++ src/main/java/fr/umontpellier/iut/bang/Test.java
@@ -1,16 +1,25 @@
 package fr.umontpellier.iut.bang;
 
 import javafx.application.Application;
-import javafx.css.Style;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 public class Test extends Application {
     private BorderPane root;
     private Pane panneau;
     private Scene scene;
+    private HBox haut;
 
     public static void main(String[] args) {
         launch(args);
@@ -19,18 +28,31 @@ public class Test extends Application {
     public void start(Stage primaryStage) throws Exception {
         root=new BorderPane();
         root.setMaxWidth(1500);
-        root.setMaxHeight(750);
+        root.setMaxHeight(1000);
 
         panneau=new Pane();
+        ImageView logo = new ImageView("ressources/logo.png");
+        ImageView titre = new ImageView("ressources/titre.png");
+        haut = new HBox();
+
+        logo.setFitWidth(200);
+        logo.setFitHeight(200);
+        titre.setFitHeight(200);
+        haut.getChildren().addAll(logo, titre);
+        haut.setAlignment(Pos.CENTER);
+        haut.setPadding(new Insets(50,0,0,0));
+
         root.setCenter(panneau);
         root.setId("pane");
+        root.setTop(haut);
         scene = new Scene(root);
-
         scene.getStylesheets().add(this.getClass().getClassLoader().getResource("src/main/resources/Css/accueil.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.setTitle("Bang");
-        primaryStage.setHeight(750);
-        primaryStage.setWidth(1500);
+        primaryStage.setHeight(700);
+        primaryStage.setWidth(1250);
         primaryStage.show();
+
+
     }
 }
