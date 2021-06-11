575abe03d6ad4c9a0fb0aecc014d57c7a57dbf73
Delalande-Tom <tom.delalande@etu.umontpellier.fr>
2021-06-11 12:33:09 +0200

    Ajout consulter les règles sur jeu

--- src/main/java/fr/umontpellier/iut/bang/views/ourviews/MyMainView.java
+++ src/main/java/fr/umontpellier/iut/bang/views/ourviews/MyMainView.java
@@ -1,57 +1,79 @@
 package fr.umontpellier.iut.bang.views.ourviews;
 
 import fr.umontpellier.iut.bang.BangIHM;
+import javafx.application.Application;
+import javafx.application.HostServices;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class MyMainView extends Stage {
 
-    private ImageView logo;
-    private Pane haut;
-    private Rectangle rectangle;
 
-    public MyMainView(BangIHM bangIHM){
+    public MyMainView(BangIHM bangIHM) {
         Pane tout = new Pane();
-        Scene scene =new Scene(tout);
-
-
+        Scene scene = new Scene(tout);
 
         //background
         ImageView background = new ImageView("src/main/resources/images/background.png");
         background.setFitWidth(1500);
         background.setFitHeight(750);
         tout.getChildren().add(background);
+        //logo
+        ImageView logo = new ImageView("src/main/resources/images/logo.png");
+        logo.setFitWidth(75);
+        logo.setPreserveRatio(true);
+        tout.getChildren().add(logo);
 
         // image derriere Jeu (joueur ect...)
         MyGameView imageJeu = new MyGameView(bangIHM.getIGame());
         imageJeu.setLayoutX(75);
         imageJeu.setLayoutY(7.5);
-        rectangle = new Rectangle();
+        Rectangle rectangle = new Rectangle();
         rectangle.setWidth(1000);
         rectangle.setHeight(700);
         rectangle.setArcWidth(140);
         rectangle.setArcHeight(140);
         rectangle.setFill(Color.rgb(217, 217, 217, 0.7));
         imageJeu.getChildren().add(rectangle);
+        tout.getChildren().add(imageJeu);
 
         //Outils
         ImageView parchemin = new ImageView("src/main/resources/images/parchemin.png");
+        Pane outils = new Pane(parchemin);
         parchemin.setPreserveRatio(true);
         parchemin.setFitHeight(125);
-        parchemin.setLayoutX(1375);
-        tout.getChildren().add(parchemin);
+        outils.setLayoutX(1375);
+        //label outils
+        Text labelOutils = new Text("Outils :");
+        labelOutils.setUnderline(true);
+        labelOutils.setLayoutY(25);
+        labelOutils.setLayoutX(30);
+        outils.getChildren().add(labelOutils);
+        //label consulter les règles
+        Label rules = new Label("Règles");
+        rules.setStyle("-fx-font-weight: bold");
+        ImageView pdf = new ImageView("src/main/resources/images/pdf.png");
+        pdf.setPreserveRatio(true);
+        pdf.setFitHeight(15);
+        rules.setGraphic(pdf);
+        rules.setLayoutY(50);
+        rules.setLayoutX(25);
+        rules.setOnMouseClicked(mouseEvent -> lirePdfRegles(bangIHM));
+        outils.getChildren().add(rules);
+
+        tout.getChildren().add(outils);
 
 
 
@@ -59,21 +81,18 @@ public class MyMainView extends Stage {
 
 
 
-        //logo
-        logo = new ImageView("src/main/resources/images/logo.png");
-
-        logo.setFitWidth(75);
-        logo.setPreserveRatio(true);
-        haut = new Pane();
-        haut.getChildren().add(logo);
 
 
 
 
-        tout.getChildren().add(haut);
-        tout.getChildren().add(imageJeu);
+
         setWidth(1500);
         setHeight(750);
         setScene(scene);
+    }
+    private void lirePdfRegles(BangIHM bangIHM){
+        File file = new File("src/main/resources/pdf/Bang-regles.pdf");
+        HostServices hostServices = bangIHM.getHostServices();
+        hostServices.showDocument(file.getAbsolutePath());
     }
 }
