3f0ebe659c8a109c2bb366051b22028637bbde98
Delalande-Tom <tom.delalande@etu.umontpellier.fr>
2021-06-12 20:35:39 +0200

    ajout de MySelectionArea Vide

---
+++ src/main/java/fr/umontpellier/iut/bang/views/ourviews/MyPlayerSelectionArea.java
@@ -0,0 +1,25 @@
+package fr.umontpellier.iut.bang.views.ourviews;
+
+import fr.umontpellier.iut.bang.views.PlayerArea;
+import fr.umontpellier.iut.bang.views.PlayerSelectionArea;
+
+public class MyPlayerSelectionArea extends PlayerSelectionArea {
+    public MyPlayerSelectionArea(PlayerArea playerArea) {
+        super(playerArea);
+    }
+
+    @Override
+    public void setVisible() {
+
+    }
+
+    @Override
+    public void setUnVisible() {
+
+    }
+
+    @Override
+    protected void setPlayerSelectedListener() {
+
+    }
+}
