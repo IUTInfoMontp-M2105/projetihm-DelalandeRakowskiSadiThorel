43b5d1c86eadce2e1c14aaee88e1cbb0ab9d4d91
Maëlis-Rakowski <maelis.rakowski@etu.umontpellier.fr>
2021-06-12 13:33:43 +0200

    vue des cartes faites en cours

--- src/main/java/fr/umontpellier/iut/bang/views/ourviews/MyCardView.java
+++ src/main/java/fr/umontpellier/iut/bang/views/ourviews/MyCardView.java
@@ -1,12 +1,20 @@
 package fr.umontpellier.iut.bang.views.ourviews;
 
 import fr.umontpellier.iut.bang.ICard;
+import fr.umontpellier.iut.bang.IGame;
+import fr.umontpellier.iut.bang.IPlayer;
 import fr.umontpellier.iut.bang.views.CardView;
 import fr.umontpellier.iut.bang.views.PlayerArea;
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class MyCardView extends CardView {
     public MyCardView(ICard card, PlayerArea playerArea) {
         super(card, playerArea);
+        Label nomCarte = new Label(card.getName());
+        getChildren().add(nomCarte);
+        setCardSelectionListener();
     }
 
     @Override
@@ -21,6 +29,14 @@ public class MyCardView extends CardView {
 
     @Override
     protected void setCardSelectionListener() {
-
+        setOnMouseClicked(whenCardSelected);
     }
+
+    private EventHandler<MouseEvent> whenCardSelected = mouseEvent -> {
+        MyCardView selectedCardView = (MyCardView) mouseEvent.getSource();
+        ICard selectedCard = selectedCardView.getICard();
+        IGame currentGame = selectedCardView.getPlayerArea().getGameView().getIGame();
+        IPlayer targetPlayer = selectedCardView.getPlayerArea().getIPlayer();
+        currentGame.onCardSelection(selectedCard, targetPlayer);
+    };
 }
